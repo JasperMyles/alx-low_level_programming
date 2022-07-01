7b247be8bf2ea4e989e266dc17c91614f948a317
JasperMyles <ograhjoshua@gmail.com>
2022-07-01 02:04:56 -0700

    Useful piece

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <unistd.h>
 /**
 *main Entry point for our program using better linter
@@ -7,7 +8,7 @@ int main(void)
 {
 /* Note: You are not allowed to use print of or puts instead you are going to use write
 */
-  write(2, "and that  piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+write(2, "and that  piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
   /* insert one before and after two*/
 return (1);
 }
