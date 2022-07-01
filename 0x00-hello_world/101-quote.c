b53eb202315bbf466150c5fdba8b9497487b9780
JasperMyles <ograhjoshua@gmail.com>
2022-07-01 02:09:38 -0700

    Unix is basically a simple operating system

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,11 +4,9 @@
 *main Entry point for our program using better linter
 *Return: 0 always success
 */
+
 int main(void)
 {
-/* Note: You are not allowed to use print of or puts instead you are going to use write
-*/
 write(2, "and that  piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-  /* insert one before and after two*/
 return (1);
 }
