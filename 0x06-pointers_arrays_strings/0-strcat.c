81d0e6b68eabfc25d6c3eb1e81ec21ae31d5c756
JasperMyles <ograhjoshua@gmail.com>
2022-07-14 14:48:08 -0700

    strnt cat commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+ * *_strncat - function that concatenates two strings
+ * @dest: first parameter
+ * @src: second parameter
+ * @n: - third parameter
+ * Return: string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+  strncat(dest, src, n);
+  return (dest);
+}
