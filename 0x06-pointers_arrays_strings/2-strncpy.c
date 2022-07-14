512acdf7f627e5691fe85047c15ad9b50bd65d97
Ograh Joshua <106625785+JasperMyles@users.noreply.github.com>
2022-07-14 19:28:30 -0400

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * *_strncpy -  copies a string
- * @dest:pointer to destination of the array
- * @src:  string to be appended.
- * @n:maximum number of characters to be appended.
- * Return: a pointer to the resulting string
- */
+* *_strncpy -  copies a string
+* @dest:pointer to destination of the array
+* @src:  string to be appended.
+* @n:maximum number of characters to be appended.
+* Return: a pointer to the resulting string
+*/
 char *_strncpy(char *dest, char *src, int n)
 {
-  int i = 0;
+int i = 0;
 
-  while (src[i] != '\0' && i < n)
-    {
-      dest[i]  = src[i];
-      i++;
-    }
-  while (i < n)
-    {
-      dest[i] = '\0';
-      i++;
-    }
-  return (dest);
+while (src[i] != '\0' && i < n)
+{
+dest[i]  = src[i];
+i++;
+}
+while (i < n)
+{
+dest[i] = '\0';
+i++;
+}
+return (dest);
 }
