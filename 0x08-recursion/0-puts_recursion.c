68e5e18000bfae1ff98c3130592a4743e96044ac
JasperMyles <ograhjoshua@gmail.com>
2022-07-20 13:19:34 -0700

    String

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *main - prints a string, followed by a newline.
+ *
+ *Return: (0) success.
+ */
+
+void _puts_recursion(char *s)
+{
+  puts("Puts with recursion\n");
+  return (0);
+}
